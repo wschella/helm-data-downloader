acfde11db330f4a8cd0a4fee50e16bd971ef5304
Wout Schellaert <wout.schellaert@gmail.com>
2023-09-11 17:20:23 +0200

    Add helm data downloader script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# helm-data-downloader
-Download evaluation data from the Stanford HELM benchmarking effort
+# HELM Data Downloader
+
+Download evaluation data from the Stanford HELM project
+
+At the time of writing, the HELM evaluation effort contains almost 7000 evaluation runs, totalling about 70GiB of prompts, model outputs, and meta data.
+
+This script/tool allows you to download it all easily.
+
+## Usage
+
+Go to [https://crfm.stanford.edu/helm/latest/?runs=1](https://crfm.stanford.edu/helm/latest/?runs=1) and download the HTLM, which contains the list off all runs.
+
+Run the downloader:
+
+```shell
+$ helm-data-downloader --source-file helm.html
+Found 6879 runs online. No runs already downloaded found. Downloading all.
+  2%|██▋              | 171/6879 [07:05<4:56
+  3%|██▋              | 172/6879 [07:07<4:53
+  3%|██▋              | 173/6879 [07:10<4:45
+  3%|██▊...
+```
+
+You might need to specify the storage API to get the latest results,
+we currently hardcode `https://storage.googleapis.com/crfm-helm-public/benchmark_output/runs/v0.2.3/` to avoid having to run selenium. Find out by clicking on e.g. `Full JSON` on a run info page like [this one](https://crfm.stanford.edu/helm/latest/?runs=1&runSpec=babi_qa%3Atask%3D15%2Cmodel%3Dtogether_glm%2Cstop%3Dhash), and set the correct storage URL with `--storage-url`.
---
+++ src/helm_data_downloader/download.py
@@ -0,0 +1,95 @@
+from typing import *
+from pathlib import Path
+from dataclasses import dataclass
+import urllib.parse
+
+import requests
+from tqdm import tqdm
+from pyquery import PyQuery as pq
+
+@dataclass
+class Args:
+    """CLI Arguments"""
+    source_url: str
+    source_file: Optional[Path]
+    storage_url: str
+    output_dir: Path
+    redownload: bool
+    max_runs: Optional[int]
+    # https://storage.googleapis.com/crfm-helm-public/benchmark_output/runs/v0.2.3/babi_qa:task=15,model=AlephAlpha_luminous-base/run_spec.json
+
+@dataclass
+class RunInfo():
+    id: str
+    href: str
+    adaptation_method: str
+
+def run(args: Args):
+    # session = HTMLSession()
+    # page = session.get(args.source_url, params={"runs": 1})
+    # page.html.render()
+
+    if args.source_file is not None:
+        page = args.source_file.read_text()
+    else:
+        page = requests.get(args.source_url, {"runs": 1}).text
+
+    page = pq(page)
+
+    # Find all runs in the page
+    runs_raw = page("table.query-table tr") # Get all rows of the run table
+    runs_raw = runs_raw[1:] # Ignore the heading
+    runs: List[RunInfo] = [RunInfo(
+        id=str(pq(run[0][0]).text()), # Get first column, first element (a link), and get the text
+        href=str(pq(run[0][0]).attr('href')), # Get first column, first element (a link), and get the href
+        adaptation_method=str(pq(run[1]).text()) # Get second column and get the text
+    ) for run in runs_raw]
+
+    already_downloaded = set(run.id for run in runs if (args.output_dir / urllib.parse.quote(run.id, safe="")).exists())
+    # TODO Filter out runs that don't have all three files present
+    runs_to_download = [run for run in runs if run.id not in already_downloaded]
+    if len(already_downloaded) > 0 and not args.redownload:
+        print(f"Found {len(runs)} runs online, and {len(already_downloaded)} runs already downloaded.\n" + \
+              f"Downloading remaining {len(runs_to_download)}.")
+    else:
+        if args.redownload:
+            print(f"Found {len(runs)} runs online, and {len(already_downloaded)} runs already downloaded.\n" + \
+                  f"Redownload flag set. Downloading all.")
+        else:
+            print(f"Found {len(runs)} runs online. No runs already downloaded found. Downloading all.")
+
+
+    args.output_dir.mkdir(parents=True, exist_ok=True)
+    for run in tqdm(runs_to_download[:args.max_runs]):
+        path_safe_run_id = urllib.parse.quote(run.id, safe="")
+        run_dir_path = args.output_dir / path_safe_run_id
+        run_dir_path.mkdir(parents=True, exist_ok=True)
+    
+        run_spec = requests.get(f"{args.storage_url}{run.id}/run_spec.json")
+        scenario_state = requests.get(f"{args.storage_url}{run.id}/scenario_state.json")
+        scenario = requests.get(f"{args.storage_url}{run.id}/scenario.json")
+
+        with open(run_dir_path / "run_spec.json", "wb") as f:
+            f.write(run_spec.content)
+        with open(run_dir_path / "scenario_state.json", "wb") as f:
+            f.write(scenario_state.content)
+        with open(run_dir_path / "scenario.json", "wb") as f:
+            f.write(scenario.content)
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="HELM Data Downloader")
+    parser.add_argument("--source-url", type=str, default="https://crfm.stanford.edu/helm/latest/", help="Source URL to download data from.")
+    parser.add_argument("--source-file", type=Path, help="Already downloaded source file with all raw runs.")
+    parser.add_argument("--storage-url", type=str, default="https://storage.googleapis.com/crfm-helm-public/benchmark_output/runs/v0.2.3/", help="Storage URL to download run data from.")
+    parser.add_argument("--output-dir", type=Path, default="data", help="Output directory to store downloaded data.")
+    parser.add_argument("--redownload", action="store_true", help="Redownload all data, even if present already.")
+    parser.add_argument("--max-runs", type=int, default=None, help="Maximum number of runs to download.")
+
+    args = parser.parse_args()
+
+    run(Args(**vars(args)))
+
+if __name__ == "__main__":
+    main()
