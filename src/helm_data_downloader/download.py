4b7aa537727aab4a64a43f1a6d433f1d80bd59b2
Wout Schellaert <wout.schellaert@gmail.com>
2023-12-01 15:13:22 +0100

    Apply Ruff formatting

--- src/helm_data_downloader/download.py
+++ src/helm_data_downloader/download.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 @dataclass
 class Args:
     """CLI Arguments"""
+
     release: str
     output_dir: Path
     storage_url: Optional[str]
@@ -21,11 +22,12 @@ class Args:
 
 
 @dataclass
-class RunInfo():
+class RunInfo:
     id: str
 
     def path_safe_id(self):
         return urllib.parse.quote(self.id, safe="")
+
 
 def run(args: Args):
     """
@@ -33,7 +35,7 @@ def run(args: Args):
 
     The latest release of the benchmarking website is at
      <https://crfm.stanford.edu/helm/latest/#/runs>.
-    The corresponding semver string is provided in 
+    The corresponding semver string is provided in
       <https://crfm.stanford.edu/helm/latest/config.js>, which looks like:
         ```
         window.BENCHMARK_OUTPUT_BASE_URL = "https://storage.googleapis.com/crfm-helm-public/";
@@ -43,11 +45,11 @@ def run(args: Args):
     The list of all runs is available at
       <{BASE_URL}/benchmark_output/runs/{RELEASE}/run_specs.json>,
       where {RELEASE} is the semver string, e.g. v0.2.4.
-    The data for each individual run is available at 
+    The data for each individual run is available at
       <{BASE_URL}/benchmark_output/runs/{RELEASE}/{RUN_ID}/scenario_state.json>,
       where {RUN_ID} is a run id babi_qa:task=15,model=AlephAlpha_luminous-base,
       which is sourced from the run_specs.json.
-    The latest mapping from run ids to run suites is available at 
+    The latest mapping from run ids to run suites is available at
       <{BASE_URL}/benchmark_output/runs/{RELEASE}/runs_to_run_suites.json>.
 
     A complete example of all instance results is this:
@@ -63,11 +65,15 @@ def run(args: Args):
             # window.SUITE = null;
             # ```
             config_js = requests.get("https://crfm.stanford.edu/helm/latest/config.js")
-            semver = re.search(r'window.RELEASE = "(v\d+\.\d+\.\d+)";',
-                               config_js.text).group(1)  # type: ignore
+            semver = re.search(
+                r'window.RELEASE = "(v\d+\.\d+\.\d+)";',
+                config_js.text,
+            ).group(1)  # type: ignore
             print(f"Using latest release, which is found to be '{semver}'.")
         except Exception:
-            print("Could not find latest release automatically. Try setting it manually with e.g. `--release v0.2.4`.")
+            print(
+                "Could not find latest release automatically. Try setting it manually with e.g. `--release v0.2.4`."
+            )
             sys.exit(1)
     else:
         semver = args.release
@@ -76,14 +82,16 @@ def run(args: Args):
     if args.storage_url is None:
         try:
             config_js = requests.get("https://crfm.stanford.edu/helm/latest/config.js")
-            storage_url = re.search(r'window.BENCHMARK_OUTPUT_BASE_URL =\s+"(.*)";',
-                                    config_js.text).group(1)  # type: ignore
+            storage_url = re.search(
+                r'window.BENCHMARK_OUTPUT_BASE_URL =\s+"(.*)";',
+                config_js.text,
+            ).group(1)  # type: ignore
             print(f"Found storage URL '{storage_url}'.")
         except Exception:
             default = "https://storage.googleapis.com/crfm-helm-public/"
             print("Could not find storage URL automatically")
             print(f"Using default URL '{default}'")
-            print("You can also try setting it manually with e.g. `--storage-url https://storage.googleapis.com/crfm-helm-public/`.")
+            print("You can also try setting it manually with e.g. `--storage-url`.")
             storage_url = default
     else:
         storage_url = args.storage_url
@@ -101,28 +109,31 @@ def run(args: Args):
     runs = [RunInfo(id=run_spec["name"]) for run_spec in run_specs]
 
     # Manage already downloaded runs
-    already_downloaded = set(run.id for run in runs if (
-        (output_dir / run.path_safe_id()).exists() and
-        (output_dir / run.path_safe_id() / "run_spec.json").exists() and
-        (output_dir / run.path_safe_id() / "scenario_state.json").exists() and
-        (output_dir / run.path_safe_id() / "scenario.json").exists()
-    ))
+    already_downloaded = set(
+        run.id
+        for run in runs
+        if (
+            (output_dir / run.path_safe_id()).exists()
+            and (output_dir / run.path_safe_id() / "run_spec.json").exists()
+            and (output_dir / run.path_safe_id() / "scenario_state.json").exists()
+            and (output_dir / run.path_safe_id() / "scenario.json").exists()
+        )
+    )
     runs_to_download = [run for run in runs if run.id not in already_downloaded]
-    dry_run_note = " Note: dry run." if args.dry_run else ""
-    if len(already_downloaded) > 0 and not args.redownload:
-        print(f"Found {len(runs)} runs online, and {len(already_downloaded)} runs already downloaded.\n" +
-              f"Downloading remaining {len(runs_to_download)}.{dry_run_note}")
+    print(f"Found {len(runs)} runs online.", end="")
+    print(f"Found {len(already_downloaded)} runs already downloaded.")
+    if args.redownload:
+        print(f"Redownload flag set. Downloading all {len(runs_to_download)} runs.")
     else:
-        if args.redownload:
-            print(f"Found {len(runs)} runs online, and {len(already_downloaded)} runs already downloaded.\n" +
-                  f"Redownload flag set. Downloading all.{dry_run_note}")
-        else:
-            print(f"Found {len(runs)} runs online. No runs already downloaded found. " +
-                  f"Downloading all.{dry_run_note}")
+        print(f"Downloading remaining {len(runs_to_download)} runs.")
+    if args.max_runs is not None:
+        print(f"NOTE: Capped at {args.max_runs} runs, by --max-runs.")
+    if args.dry_run:
+        print("NOTE: Dry run. Not downloading any runs.")
 
     # Download runs
     output_dir.mkdir(parents=True, exist_ok=True)
-    for run in tqdm(runs_to_download[:args.max_runs]):
+    for run in tqdm(runs_to_download[: args.max_runs]):
         if args.dry_run:
             continue
 
@@ -130,7 +141,7 @@ def run(args: Args):
         run_dir_path.mkdir(parents=True, exist_ok=True)
 
         run_spec = requests.get(f"{storage_url}/runs/{run.id}/run_spec.json")
-        scenario_state = requests.get(f"{storage_url}/runs/{run.id}/scenario_state.json")
+        scenario_state = requests.get(f"{storage_url}/runs/{run.id}/scenario_state.json")  # fmt: skip
         scenario = requests.get(f"{storage_url}/runs/{run.id}/scenario.json")
 
         with open(run_dir_path / "run_spec.json", "wb") as f:
@@ -145,21 +156,38 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(description="HELM Data Downloader")
-    parser.add_argument("--release", type=str, default="latest",
-                        help="HELM release version to download data from. Example: v0.2.4. The default is 'latest'.")
-    parser.add_argument("--output-dir", type=Path, default=None,
-                        help="Output directory to store downloaded data. Default: ./data/{RELEASE}/")
-    parser.add_argument("--storage-url", type=str, default=None,
-                        help="The URL to download data from. Default behaviour is to search for it on the HELM website, " +
-                             "and if not found, use default value <https://storage.googleapis.com/crfm-helm-public/>." +
-                             "It can be changed to e.g. use local mirror with similar folder structure, " +
-                             "or adapted when HELM changes their storage location and this tool has not been updated yet.")
-    parser.add_argument("--redownload", action="store_true",
-                        help="Redownload all data, even if present already.")
-    parser.add_argument("--max-runs", type=int, default=None,
-                        help="Maximum number of runs to download.")
-    parser.add_argument("--dry-run", action="store_true",
-                        help="Dry run. Do not download any runs.")
+    parser.add_argument(
+        "--release",
+        type=str,
+        default="latest",
+        help="HELM release version to download data from. Example: v0.2.4. The default is 'latest'.",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        default=None,
+        help="Output directory to store downloaded data. Default: ./data/{RELEASE}/",
+    )
+    parser.add_argument(
+        "--storage-url",
+        type=str,
+        default=None,
+        help="The URL to download data from. Default behaviour is to search for it on the HELM website, "
+        + "and if not found, use default value <https://storage.googleapis.com/crfm-helm-public/>."
+        + "It can be changed to e.g. use local mirror with similar folder structure, "
+        + "or adapted when HELM changes their storage location and this tool has not been updated yet.",
+    )
+    parser.add_argument(
+        "--redownload",
+        action="store_true",
+        help="Redownload all data, even if present already.",
+    )
+    parser.add_argument(
+        "--max-runs", type=int, default=None, help="Maximum number of runs to download."
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", help="Dry run. Do not download any runs."
+    )
 
     args = parser.parse_args()
 
