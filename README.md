3db2974b643499e40235e5986d34d6da5302bc57
Wout Schellaert <wout.schellaert@gmail.com>
2024-04-23 17:22:45 +0200

    Support all HELM projects

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HELM and HEIM Data Downloader
 
-Download evaluation data from the Stanford _Holistic Evaluation of Language Models (HELM)_ and _Holistic Evaluation of Text-To-Image Models (HEIM)_ projects.
+Download evaluation data from the Stanford _Holistic Evaluation of Language Models (HELM)_ project, including _HELM Lite_, _HELM Instruct_, and _HEIM_.
 
 At the time of writing, the HELM evaluation effort is at release v0.4.0 and contains more than almost 8500 evaluation runs, totalling more than 800GiB of prompts, model outputs, and meta data.
 
@@ -25,13 +25,13 @@ Found 8526 runs online. No runs already downloaded found. Downloading all.
   3%|██▊...
 ```
 
-Same goes for HEIM, just use `heimdd` instead of `helmdd`.
+To download _HELM Lite_ data, just use `--project lite`, same goes for `heim` and `instruct`.
 
 ### Options
 
 ```shell
 $ helmdd --help
-usage: helmdd [-h] [--release RELEASE] [--output-dir OUTPUT_DIR]
+usage: helmdd [-h] [--project PROJECT_ID] [--release RELEASE] [--output-dir OUTPUT_DIR]
               [--storage-url STORAGE_URL] [--redownload] [--max-runs MAX_RUNS]
               [--dry-run] [--files FILES [FILES ...]]
 
@@ -39,28 +39,28 @@ HELM Data Downloader
 
 options:
   -h, --help            show this help message and exit
-  --release RELEASE     HELM release version to download data from. Example: v0.2.4.
-                        The default is 'latest'.
+  --project PROJECT_ID  Project to download data from. Options: classic, heim, lite,
+                        instruct, all. Default: lite.
+  --release RELEASE     Release version to download data from. Example: v0.2.4. The
+                        default is 'latest', which will search for the latest release.
   --output-dir OUTPUT_DIR
                         Output directory to store downloaded data. Default: ./helm-
-                        data/<RELEASE>/
+                        data/<PROJECT>/<RELEASE>/
   --storage-url STORAGE_URL
-                        The URL to download data from. Default behaviour is to
-                        search for it on the HELM website, and if not found, use
-                        default value <https://storage.googleapis.com/crfm-helm-
-                        public/>.It can be changed to e.g. use local mirror with
-                        similar folder structure, or adapted when HELM changes their
-                        storage location and this tool has not been updated yet.
+                        The URL to download data from. Default behaviour is to search
+                        for it on the HELM website.It can be changed to e.g. use local
+                        mirror with similar folder structure, or adapted when HELM
+                        changes their storage location and this tool has not been
+                        updated yet.
   --redownload          Redownload all data, even if present already.
   --max-runs MAX_RUNS   Maximum number of runs to download.
   --dry-run             Dry run. Do not download any runs.
   --files FILES [FILES ...]
-                        Files to download for each run. Default:
-                        [scenario_state.json, instances.json,
-                        display_predictions.json]. Available [run_spec.json,
-                        scenario.json, scenario_state.json, stats.json,
+                        Files to download for each run. Default: [scenario_state.json,
+                        instances.json, display_predictions.json]. Available:
+                        [run_spec.json, scenario.json, scenario_state.json, stats.json,
                         instances.json, display_predictions.json,
-                        display_requests.json]You can also put 'all' to download all
+                        display_requests.json].You can also put 'all' to download all
                         files.
 ```
 
@@ -72,3 +72,5 @@ Currently still not possible yet:
 - select which data to download (prompts, model outputs, meta data)
 
 All of this should be easy to add yourself if needed. Feel free to open a PR.
+
+Other interesting files are e.g. the schema.json, e.g. <https://storage.googleapis.com/crfm-helm-public/benchmark_output/releases/v0.4.0/schema.json>, which contains all the models, metrics, adapters, etc...
--- src/helm_data_downloader/helm.py
+++ src/helm_data_downloader/helm.py
@@ -1,19 +1,91 @@
+from typing import Callable, Optional, TypedDict, Dict
 from pathlib import Path
+from dataclasses import dataclass
 import re
 import sys
 import json
+import urllib.parse
+import argparse
 
 import requests
 from tqdm import tqdm
 
-from helm_data_downloader.shared import Args, RunInfo, get_arg_parser
+FILES = [
+    "run_spec.json",
+    "scenario.json",
+    "scenario_state.json",
+    "stats.json",
+    "instances.json",
+    "display_predictions.json",
+    "display_requests.json",
+]
+PROJECTS = ["classic", "heim", "lite", "instruct"]
+
+
+class ProjectInfo(TypedDict):
+    name: str
+    release_regex: str
+    get_release_url: Callable[[str, str], str]
+
+
+PROJECT_INFO: Dict[str, ProjectInfo] = {
+    "classic": {
+        "name": "Classic",
+        "release_regex": r'window.RELEASE = "(.+)";',
+        "get_release_url": lambda storage_url, release: f"{storage_url}/releases/{release}",
+    },
+    "heim": {
+        "name": "HEIM",
+        "release_regex": r'window.SUITE = "(.+)";',
+        "get_release_url": lambda storage_url, release: f"{storage_url}/runs/{release}",
+    },
+    "lite": {
+        "name": "Lite",
+        "release_regex": r'window.RELEASE = "(.+)";',
+        "get_release_url": lambda storage_url, release: f"{storage_url}/releases/{release}",
+    },
+    "instruct": {
+        "name": "Instruct",
+        "release_regex": r'window.RELEASE = "(.+)";',
+        "get_release_url": lambda storage_url, release: f"{storage_url}/releases/{release}",
+    },
+}
+
+
+@dataclass
+class Args:
+    """CLI Arguments"""
+
+    project_id: str
+    release: str
+    output_dir: Path
+    storage_url: Optional[str]
+    redownload: bool
+    max_runs: Optional[int]
+    dry_run: bool
+    files: list[str]
+
+
+@dataclass
+class RunInfo:
+    id: str
+    suite: str
+
+    def path_safe_id(self):
+        return urllib.parse.quote(self.id, safe="")
 
 
 def run(args: Args):
+    projects = PROJECTS if args.project_id == "all" else [args.project_id]
+    for project_id in projects:
+        download_project(project_id, args)
+
+
+def download_project(project: str, args: Args):
     """
     Download all runs from the HELM benchmarking website.
 
-    The latest release of the benchmarking website is at
+    The latest release of the benchmarking website is e.g. at
      <https://crfm.stanford.edu/helm/classic/latest/#/runs>.
     The corresponding semver string is provided in
       <https://crfm.stanford.edu/helm/classic/latest/config.js>, which looks like:
@@ -39,15 +111,19 @@ def run(args: Args):
     A complete example of all instance results is this:
      https://storage.googleapis.com/crfm-helm-public/benchmark_output/runs/v0.2.3/babi_qa:task=15,model=AlephAlpha_luminous-base/run_spec.json
     """
-    # Get semver
+    if project not in PROJECTS:
+        raise ValueError(f"Unknown HELM project: {project}.")
+
+    print(f"# Downloading data for HELM {PROJECT_INFO[project]['name']} project.")
+    project_info = PROJECT_INFO[project]
+    config_url = f"https://crfm.stanford.edu/helm/{project}/latest/config.js"
+
+    # Get version
     if args.release == "latest":
         try:
-            # See docstring for more info.
-            config_js = requests.get("https://crfm.stanford.edu/helm/classic/latest/config.js")  # fmt: skip
-            release = re.search(
-                r'window.RELEASE = "(v\d+\.\d+\.\d+)";',
-                config_js.text,
-            ).group(1)  # type: ignore
+            config_js = requests.get(config_url)
+            release_re = re.search(project_info["release_regex"], config_js.text)
+            release = release_re.group(1)  # type: ignore
             print(f"Using latest release, which is found to be '{release}'.")
         except Exception:
             print("Could not find latest release automatically. ", end="")
@@ -59,29 +135,26 @@ def run(args: Args):
     # Get storage url
     if args.storage_url is None:
         try:
-            config_js = requests.get("https://crfm.stanford.edu/helm/classic/latest/config.js")  # fmt: skip
-            storage_url = re.search(
+            config_js = requests.get(config_url)
+            storage_url_re = re.search(
                 r'window.BENCHMARK_OUTPUT_BASE_URL =\s+"(.*)";',
                 config_js.text,
-            ).group(1)  # type: ignore
+            )
+            storage_url = storage_url_re.group(1)  # type: ignore
             print(f"Found storage URL '{storage_url}'.")
         except Exception:
-            default = "https://storage.googleapis.com/crfm-helm-public/"
-            print("Could not find storage URL automatically")
-            print(f"Using default URL '{default}'")
-            print("You can also try setting it manually with e.g. `--storage-url`.")
-            storage_url = default
+            print("Could not find storage URL automatically. ", end="")
+            print("Try setting it manually with e.g. `--storage-url https://example.com`.")
     else:
         storage_url = args.storage_url
-        print(f"Using manually provided storage URL '{storage_url}'.")
 
     # Normalise storage url
     storage_url = storage_url.rstrip("/")
     storage_url = f"{storage_url}"
-    release_url = f"{storage_url}/releases/{release}"
+    release_url = project_info["get_release_url"](storage_url, release)
 
     # Get run ids
-    print(f"Getting run ids from {release_url}/run_specs.json")
+    print(f"Getting run ids from '{release_url}/run_specs.json")
     try:
         run_specs = requests.get(f"{release_url}/run_specs.json").json()
     except json.JSONDecodeError as e:
@@ -94,22 +167,17 @@ def run(args: Args):
     run_ids = [run_spec["name"] for run_spec in run_specs]
 
     # Get run to suite mapping
-    print(f"Getting run to suite mapping from {release_url}/runs_to_run_suites.json")  # fmt: skip
-    runs_to_run_suites = requests.get(f"{release_url}/runs_to_run_suites.json").json()  # fmt: skip
-    runs = [RunInfo(id=id, suite=runs_to_run_suites[id]) for id in run_ids]
+    if project != "heim":
+        print(f"Getting run to suite mapping from {release_url}/runs_to_run_suites.json")  # fmt: skip
+        runs_to_run_suites = requests.get(f"{release_url}/runs_to_run_suites.json").json()  # fmt: skip
+        runs = [RunInfo(id=id, suite=runs_to_run_suites[id]) for id in run_ids]
+    else:
+        runs = [RunInfo(id=id, suite=release) for id in run_ids]
 
-    output_dir = args.output_dir or Path("./helm-data/") / release
-
-    # All the different files associated with a run
-    # Options:
-    # "run_spec.json",
-    # "scenario.json",
-    # "scenario_state.json",
-    # "stats.json",
-    # "instances.json",  # !Important! Has inputs
-    # "display_predictions.json",  # !Important! Has outputs and metrics
-    # "display_requests.json",
-    run_files = args.get_files()
+    # Configure outputs
+    output_dir = args.output_dir or Path(f"./helm-data/{project}/{release}/")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    run_files = FILES if args.files == "all" else args.files
 
     # Manage already downloaded runs
     def is_downloaded(run: RunInfo):
@@ -152,13 +220,70 @@ def run(args: Args):
                 raise Exception(f"Could not download {file_name} from {file_url}.")
 
 
-def main():
-    parser = get_arg_parser(
-        benchmark="HELM",
-        storage_url="https://storage.googleapis.com/crfm-helm-public/",
+def get_parser():
+    parser = argparse.ArgumentParser(description="HELM Data Downloader")
+
+    parser.add_argument(
+        "--project",
+        dest="project_id",
+        type=str,
+        default="classic",
+        help=f"Project to download data from. Options: {', '.join(PROJECTS)}, all. Default: lite.",
     )
-    args = parser.parse_args()
-    run(Args(**vars(args)))
+
+    parser.add_argument(
+        "--release",
+        type=str,
+        default="latest",
+        help="Release version to download data from. Example: v0.2.4. The default is 'latest', which will search for the latest release.",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        default=None,
+        help="Output directory to store downloaded data. Default: ./helm-data/<PROJECT>/<RELEASE>/",
+    )
+    parser.add_argument(
+        "--storage-url",
+        type=str,
+        default=None,
+        help="The URL to download data from. Default behaviour is to search for it on the HELM website."
+        + "It can be changed to e.g. use local mirror with similar folder structure, "
+        + "or adapted when HELM changes their storage location and this tool has not been updated yet.",
+    )
+    parser.add_argument(
+        "--redownload",
+        action="store_true",
+        help="Redownload all data, even if present already.",
+    )
+    parser.add_argument(
+        "--max-runs", type=int, default=None, help="Maximum number of runs to download."
+    )
+    parser.add_argument("--dry-run", action="store_true", help="Dry run. Do not download any runs.")
+
+    default_files = [
+        "scenario_state.json",
+        "instances.json",
+        "display_predictions.json",
+    ]
+    parser.add_argument(
+        "--files",
+        type=str,
+        nargs="+",
+        default=default_files,
+        help="Files to download for each run. "
+        + f"Default: [{', '.join(default_files)}]. "
+        + f"Available: [{', '.join(FILES)}]."
+        + "You can also put 'all' to download all files.",
+    )
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args_raw = parser.parse_args()
+    args = Args(**vars(args_raw))
+    run(args)
 
 
 if __name__ == "__main__":
